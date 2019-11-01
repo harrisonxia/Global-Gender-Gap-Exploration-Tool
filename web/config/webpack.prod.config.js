c557780331a78abf4643f08a82fb6fb229981143
harrisonx <engcxia@gmail.com>
2019-10-31 19:25:18 -0700

    new minification tool for webpack4

--- web/config/webpack.prod.config.js
+++ web/config/webpack.prod.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 // const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const webpackBaseConfig = require('./webpack.common.config.js')
 const path = require('path')
@@ -8,22 +9,38 @@ const context = path.resolve(__dirname, '..')
 const ASSET_PATH = process.env.ASSET_PATH || '/CMPT767/'
 
 module.exports = merge(webpackBaseConfig, {
-    optimization: {
-        minimizer: [
-            // TODO: uglify was causing prod build issue
-            // new UglifyJsPlugin(),
-            new OptimizeCSSAssetsPlugin(),
-        ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
     },
-    output: {
-        path: path.resolve(context, 'docs'),
-        filename: '[name].[hash].js',
-        publicPath: ASSET_PATH,
-    },
-    plugins: [
-        // This makes it possible for us to safely use env vars on our code
-        new webpack.DefinePlugin({
-            'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
-        }),
+    minimize: true,
+    minimizer: [
+      // TODO: uglify was causing prod build issue
+      //   new UglifyJsPlugin(),
+      //   new UglifyJsPlugin({
+      //     cache: true,
+      //     parallel: true,
+      //     sourceMap: true,
+      //     uglifyOptions: {
+      //       compress: {
+      //         drop_console: true,
+      //       },
+      //     },
+      //   }),
+      new TerserPlugin(),
+      new OptimizeCSSAssetsPlugin(),
     ],
+  },
+  output: {
+    path: path.resolve(context, 'docs'),
+    filename: '[name].[hash].js',
+    publicPath: ASSET_PATH,
+  },
+  plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+    }),
+  ],
 })
